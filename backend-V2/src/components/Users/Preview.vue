c84ce0445e2a03b42e524c76ada4b742f26593d6
KOUAME DAVID <kouamedavid124@gmail.com>
2020-04-30 10:41:54 +0000

    - [x] Bug au niveau du détail d’un utilisateur

--- backend-V2/src/components/Users/Preview.vue
+++ backend-V2/src/components/Users/Preview.vue
@@ -1,146 +1,150 @@
 <template>
   <card class="card" :title="title">
-      <div class="row">
-        <div class="col-md-12">
-          <form v-if="abonnement">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Parent</label>
-                      <!---->
-                      <input
-                        class="form-control"
-                        type="text"
-                        v-bind:value="abonnement.parent ? abonnement.parent.name+' '+abonnement.parent.surname : ''"
-                        disabled
-                      />
-                      <!---->
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Année scolaire</label>
-                      <!---->
-                      <input
-                        class="form-control"
-                        type="text"
-                        v-bind:value="abonnement.anneescolaire ? abonnement.anneescolaire.libelle : ''"
-                        disabled
-                      />
-                      <!---->
-                    </div>
-                    </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Pack abonnement</label>
-                      <!---->
-                      <input
-                        class="form-control"
-                        type="text"
-                        v-bind:value="abonnement.packabonnement ? abonnement.packabonnement.libelle : ''"
-                        disabled
-                      />
-                      <!---->
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Commentaire</label>
-                      <!---->
-                      <textarea class="form-control" v-model="abonnement.commentaire" disabled></textarea>
-                      <!---->
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Nombre de sms initial</label>
-                      <input v-model="abonnement.nbre_sms_initial" class="form-control" disabled/>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Nombre de sms consomé</label>
-                      <input v-model="abonnement.nbre_sms_consomme" class="form-control" disabled/>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label class="control-label">Nombre de sms restant</label>
-                      <input v-model="abonnement.nbre_sms_restant" class="form-control" disabled/>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <table class="table table-d">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Matricule</th>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Prénom</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-          <tr v-if="eleve" v-for="(eleve, index) in eleves">
-            <th scope="row">{{ index + 1}}</th>
-            <td>{{ eleve.matricule  }}</td>
-            <td>{{ eleve.name  }}</td>
-            <td>{{ eleve.surname  }}</td>
-          </tr>
-        </tbody>
-                  </table>
-                </div>
-                <div class="clearfix"></div>
-                <a href="#/abonnements" class="btn btn-danger float-right">Retour</a>
-              </form>
-        </div>
+    <div class="row">
+      <!-- Show error message -->
+      <div v-if="error" class="col-md-12">
+        <message-error v-bind:error="error"></message-error>
       </div>
+
+      <div class="col-md-12" v-if="user">
+        <form v-on:submit="saveUser">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label class="control-label">
+                  Nom et prénoms (
+                  <span class="span-required">*</span>)
+                </label>
+                <!---->
+                <input type="text" class="form-control" v-model="user.name" disabled/>
+                <!---->
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label class="control-label">
+                  Email (
+                  <span class="span-required">*</span>)
+                </label>
+                <!---->
+                <input type="email" class="form-control" v-model="user.email" disabled/>
+                <!---->
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label class="control-label">
+                  Password (
+                  <span class="span-required">*</span>)
+                </label>
+                <!---->
+                <input type="password" class="form-control" v-model="password" disabled/>
+                <!---->
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label class="control-label">
+                  Confirmation password (
+                  <span class="span-required">*</span>)
+                </label>
+                <!---->
+                <input type="password" class="form-control" v-model="password_confirmation" disabled/>
+                <!---->
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label class="control-label">
+                  Rôle (
+                  <span class="span-required">*</span>)
+                </label>
+                <!---->
+                <select v-model="user.role_id" class="form-control" disabled>
+                  <option value>Sélectionnez un rôle</option>
+                  <option :value="role.id" v-for="role in roles">{{ role.libelle }}</option>
+                </select>
+                <!---->
+              </div>
+            </div>
+          </div>
+          <div class="clearfix"></div>
+          <a href="#/users" class="btn btn-danger float-right">Retour</a>
+        </form>
+      </div>
+    </div>
   </card>
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   data() {
     return {
-      title: "Détail abonnement",
+      title: "Détail un user",
+      showModal: false,
+      userId: 0,
       isLoader: false,
-      eleves: [],
+      valueDisabled: false,
+      error: null,
+      password: null,
+      password_confirmation: null
     };
   },
   created() {
-    this.$store.dispatch("getAbonnement", {
-      abonnementId: this.$route.params.id
+    this.$store.dispatch("getUser", {
+      id: this.$route.params.id
     });
-    // recuperation des eleves de l'abonnements
-    this.$store.dispatch('getAllElevesAbonnement', {abonnementId: this.$route.params.id})
+
+    // Recuperation des rôles
+    this.$store.dispatch('getRolesAll', {payload: 0, search : [
+      {key: "school_id", value: this.$cookies.get('ecoleId')},
+      {key: "annee_scolaire_id", value: this.$cookies.get('anneeScolaireId')}]})
   },
-  computed:{
-    abonnement() {
-      return this.$store.getters.abonnement;
-    },
-    elevesComputed(){
-      return this.$store.getters.elevesabonnement
+  methods: {
+    saveUser() {
+      this.valueDisabled = true;
+      let data = {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.password,
+        password_confirmation: this.password_confirmation,
+        role_id: this.user.role_id,
+        ecole_id: this.$cookies.get("ecoleId"),
+        annee_scolaire_id: this.$cookies.get("anneeScolaireId"),
+        is_admin: 1
+      };
+      let store = this.$store;
+      store
+        .dispatch("updateModel", {"url": "users", "data": data, "id": this.$route.params.id})
+        .then(response => {
+          alert("L'enregistrement a été éffectué avec succès");
+          this.$router.go(-1);
+        })
+        .catch(error => {
+          this.error = error;
+          this.errored = true;
+          this.valueDisabled = false;
+        })
+        .finally(() => (this.loading = false));
     }
   },
-  watch:{
-    eleve(){
-      this.eleves.push(this.eleve)
+  computed: {
+    user() {
+      return this.$store.getters.user;
     },
-    elevesComputed(){
-      if(this.elevesComputed && !this.isLoader){
-        var i = 0
-        for(i = 0; i < this.elevesComputed.length; i++){
-          this.eleves.push(this.elevesComputed[i])
-        }
-        this.isLoader = true
-      }
+    showEmptySentenceUser() {
+      return this.notEmptyObject(this.users) == 0;
+    },
+    roles(){
+      return this.$store.getters.roles;
     }
-  }
+  },
+  watch: {}
 };
 </script>
